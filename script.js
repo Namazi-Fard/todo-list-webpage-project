ce1585789b8aa858168a8091b96928d04a7b451e
Mohammad Hossein <Polotou2025@gmail.com>
2025-12-07 20:58:14 +0330

    add css + little js

---
+++ script.js
@@ -0,0 +1,62 @@
+const themeSwitcherBtn = document.getElementById("theme-switcher");
+const bodyTag = document.querySelector("body");
+const addBtn = document.getElementById("add-btn");
+
+function main () {
+  // Theme-Switcher
+  themeSwitcherBtn.addEventListener("click", () => {
+    bodyTag.classList.toggle("light");
+    const themeImg = themeSwitcherBtn.children[0];
+    themeImg.setAttribute(
+      "src",
+      themeImg.getAttribute("src") === "./assets/images/icon-sun.svg"
+        ? "./assets/images/icon-moon.svg"
+        : "./assets/images/icon-sun.svg"
+    );
+  });
+}
+//Add Todo In LocalStorage
+  addBtn.addEventListener("click", () => {
+    const item = todoInput.value.trim();
+    if (item) {
+      todoInput.value = "";
+      const todos = !localStorage.getItem("todos")
+        ? []
+        : JSON.parse(localStorage.getItem("todos"));
+
+      const currentTodo = {
+        item: item,
+        isCompleted: false,
+      };
+
+      todos.push(currentTodo);
+      localStorage.setItem("todos", JSON.stringify(todos));
+      makeTodoElement([currentTodo]);
+    }
+  }); 
+
+
+
+
+
+
+
+
+
+
+
+
+// const themeSwitcherBtn = document.getElementById("theme-switcher");
+// const bodyTag = document.querySelector("body");
+
+// function main() {
+//   themeSwitcherBtn.addEventListener("click", () => {
+//     bodyTag.classList.toggle("light");
+//   });
+//   const themeImg = themeSwitcherBtn.children[0];
+//   themeImg.setAttribute("src",
+//     themeImg.getAttribute("src") === "./assets/images/icon-sun.svg"?"./assets/images/icon-moon.svg":"./assets/images/icon-sun.svg"
+//   );
+// }
+
+document.addEventListener("DOMContentLoaded", main);
